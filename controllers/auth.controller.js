312ffef47d056d0d537a8315e60118520d6e146c
Yash Maharjan <yash.maharjan@yipl.com.np>
2025-08-09 10:05:46 +0545

    feat:Sign Up & Sign In Functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,8 +50,39 @@ export const signUp = async (req, res, next) => {
   }
 };
 
-export const signIn = async (req, res) => {
-  res.send("Helloe");
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      const error = new Error("Incorrect Password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User Logged In Successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signOut = async (req, res) => {
